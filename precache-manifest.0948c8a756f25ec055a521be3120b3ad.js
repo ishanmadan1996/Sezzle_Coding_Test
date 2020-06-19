688e59f7251295adf347683f8f757b1761e63232
ishanmadan1996 <ishanmadan1996@gmail.com>
2020-06-19 09:22:59 -0700

    Updates

---
+++ precache-manifest.0948c8a756f25ec055a521be3120b3ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76b6b5ebb2b4048aafe98c0585989d7",
+    "url": "/sezzle-collab-calc/index.html"
+  },
+  {
+    "revision": "be3c1a005e3b3301e8c2",
+    "url": "/sezzle-collab-calc/static/css/main.46e65095.chunk.css"
+  },
+  {
+    "revision": "e03f5269298e7e87d2cc",
+    "url": "/sezzle-collab-calc/static/js/2.c3454666.chunk.js"
+  },
+  {
+    "revision": "b328a71fadb1e554e12bf3288db47b66",
+    "url": "/sezzle-collab-calc/static/js/2.c3454666.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be3c1a005e3b3301e8c2",
+    "url": "/sezzle-collab-calc/static/js/main.45e65e12.chunk.js"
+  },
+  {
+    "revision": "d3ba36028d01bfdd88d7",
+    "url": "/sezzle-collab-calc/static/js/runtime-main.21468571.js"
+  }
+]);
